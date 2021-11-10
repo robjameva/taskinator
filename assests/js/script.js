5dc19d12f0f94139d3e3b213a08ab86ec4d4ec0a
Robert Evanik <rob.evanik@gmail.com>
2021-11-09 21:55:50 -0500

    4.2.5 handled form submission

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,8 +1,10 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form")
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
-var createTaskHandler = function () {
+var createTaskHandler = function (event) {
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
@@ -11,7 +13,7 @@ var createTaskHandler = function () {
 
 
 
-buttonEl.addEventListener("click", createTaskHandler)
+formEl.addEventListener("submit", createTaskHandler)
 
 
 
